5f36d0e8c5a190add59903e6cbdb6a36043b09bf
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2024-01-17 17:23:44 +0100

    Remove deepEqual, not needed

--- public/js/code-blocks.js
+++ public/js/code-blocks.js
@@ -60,21 +60,6 @@ function updateResponseCodeblock(response) {
     hljs.highlightElement(document.getElementById('json-responses'));
 }
 
-function isDeepEqual(arg1, arg2) {
-    if (Object.prototype.toString.call(arg1) === Object.prototype.toString.call(arg2)) {
-        if (Object.prototype.toString.call(arg1) === '[object Object]' || Object.prototype.toString.call(arg1) === '[object Array]') {
-            if (Object.keys(arg1).length !== Object.keys(arg2).length) {
-                return false;
-            }
-            return (Object.keys(arg1).every(function (key) {
-                return deepCompare(arg1[key], arg2[key]);
-            }));
-        }
-        return (arg1 === arg2);
-    }
-    return false;
-}
-
 async function onClickCopyJsonRequest() {
     const requestElement = document.getElementById("json-requests");
     try {
